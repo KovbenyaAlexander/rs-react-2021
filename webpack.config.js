da521c7aad21beb659cddf0574621cd5534b31c1
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-07-28 21:13:28 +0300

    code refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,37 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/index.js"],
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[fullhash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[fullhash].js',
   },
   devServer: {
     port: 3000,
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
     new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.(css|less)$/,
-        use: ["style-loader", "css-loader", "less-loader"],
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
       {
         test: /\.(jpg,jpeg,png,svg)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+        use: ['file-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
           },
         },
       },
