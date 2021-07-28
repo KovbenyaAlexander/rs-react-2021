25ee619d48de92351ed89960d0cd7f02935242a5
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-07-28 20:39:37 +0300

    fix lf/crlf conflict

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./index.less";
-import React from "react";
-import { render } from "react-dom";
-import App from "./App.js";
+import './index.less';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App.js';
 
-console.log("sdfsd");
+console.log('sdfsd');
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
