b7c17b5a878777dd752e21d3501e84f9918586bd
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-07-31 16:16:29 +0300

    add local state

--- src/Components/Form.js/Form.js
+++ src/Components/Form.js/Form.js
@@ -1,56 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Form.module.css';
 
-const Form = () => (
-  <div className={css.form}>
-    <form>
-      <label htmlFor="name">
-        Name
-        <input type="text" id="name" required />
-      </label>
-      <label htmlFor="email">
-        Email
-        <input type="text" id="email" required />
-      </label>
-      <label htmlFor="date">
-        Date of birth
-        <input type="date" id="date" required />
-      </label>
+const Form = () => {
+  const initialState = {
+    name: '',
+    email: '',
+    date: '',
+    sex: '',
+    family: '',
+    isAgree: '',
+  };
 
-      <label htmlFor="select" required>
-        Sex
-        <select id="select" required>
-          <option value=""> </option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-      </label>
+  const [state, setState] = useState(initialState);
 
-      <br />
+  const onChangeHandler = (e) => {
+    const typeOfInput = e.target.id;
+    const newValue = e.target.value;
 
-      <span>Family status</span>
-      <label htmlFor="single">
-        single
-        <input type="radio" id="single" name="familyStatus" required />
-      </label>
-      <label htmlFor="married">
-        married
-        <input type="radio" id="married" name="familyStatus" />
-      </label>
-      <label htmlFor="divorced">
-        divorced
-        <input type="radio" id="divorced" name="familyStatus" />
-      </label>
+    if (typeOfInput === 'single' || typeOfInput === 'married') {
+      setState((prevState) => {
+        const PrevState = prevState;
+        return {
+          ...PrevState,
+          family: typeOfInput,
+        };
+      });
+      return;
+    }
 
-      <br />
+    setState((prevState) => {
+      const PrevState = prevState;
+      return {
+        ...PrevState,
+        [typeOfInput]: newValue,
+      };
+    });
+  };
 
-      <label htmlFor="agreement">
-        <input id="agreement" name="agreement" type="checkbox" required />I
-        agree to the Terms of Service
-      </label>
-      <input type="submit" />
-    </form>
-  </div>
-);
+  return (
+    <div className={css.form}>
+      <form>
+        <label htmlFor="name">
+          Name
+          <input
+            value={state.name}
+            onChange={(e) => {
+              onChangeHandler(e);
+            }}
+            type="text"
+            id="name"
+            name="name"
+            required
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            onChange={(e) => {
+              onChangeHandler(e);
+            }}
+          />
+        </label>
+        <label htmlFor="date">
+          Date of birth
+          <input
+            type="date"
+            id="date"
+            name="date"
+            onChange={(e) => {
+              onChangeHandler(e);
+            }}
+            required
+          />
+        </label>
+
+        <label htmlFor="sex" required>
+          Sex
+          <select
+            id="sex"
+            name="sex"
+            value={state.sex}
+            required
+            onChange={(e) => {
+              onChangeHandler(e);
+            }}
+          >
+            <option value=""> </option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </label>
+
+        <br />
+
+        <span>Family status</span>
+        <label htmlFor="single">
+          single
+          <input
+            type="radio"
+            id="single"
+            name="familyStatus"
+            required
+            onChange={(e) => {
+              onChangeHandler(e);
+            }}
+          />
+        </label>
+        <label htmlFor="married">
+          married
+          <input
+            type="radio"
+            id="married"
+            name="familyStatus"
+            onChange={(e) => {
+              onChangeHandler(e);
+            }}
+          />
+        </label>
+        <br />
+        <label htmlFor="agreement">
+          <input id="agreement" name="agreement" type="checkbox" required />I
+          agree to the Terms of Service
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Form;
