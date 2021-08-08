a92bf00720b6e664354c79dba49224222f924c4d
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-08-08 13:28:27 +0300

    fix prettierConifg

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  useTabs: false,
+  trailingComma: 'all',
+  bracketSpasing: true,
+  tabWidth: 4,
+  semi: true,
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  endOfLine: 'lf',
+};
