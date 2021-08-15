148dc8346d42fb2d50f0a74d70c411317db92cc3
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-08-15 17:05:01 +0300

    fix eslint and prettier conflict

--- src/Components/CardsList/Card/Card.js
+++ src/Components/CardsList/Card/Card.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Card.module.css';
 
-const Card = ({
-  description, title, price, img,
-}) => (
+const Card = ({ description, title, price, img }) => (
   <div className={css.container}>
     <img className={css.img} src={img} alt="img" />
     <h1 className={css.title}>{title}</h1>
