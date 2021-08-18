a5a007427dd26aaad17f30b585b2c5215c0426a1
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-08-18 17:25:51 +0300

    fix jest and testing-library errors. add npm scripts for test and test:coverage.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 const sum = (a, b) => {
   return a + b;
 };
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+  // render(<App />);
+  // screen.debug();
 });
